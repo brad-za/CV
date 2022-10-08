e735e992180d058e0728eff1337f8a44bea54e14
streetRAT <psnonlineshit@gmail.com>
2022-10-08 10:28:04 +0200

    blog navigation bug fixes

--- src/Components/Blog/Dropdown.jsx
+++ src/Components/Blog/Dropdown.jsx
@@ -7,38 +7,38 @@ const Dropdown = ({ language }) => {
       <span className="">{language.name}</span>
       <div className="absolute -right-10">
         <div
-          className={`container invisible relative flex flex-shrink flex-col divide-y divide-black rounded-xl border-solid bg-green-200 p-5 group-one-hover:visible`}
+          className={`container relative hidden flex-shrink flex-col divide-y divide-black rounded-xl border-solid bg-green-200 p-5 group-one-hover:flex`}
         >
           {language.categories.map((category) => (
-            <NavLink
-              key={category.slug}
-              to={`${language.slug}/${category.slug}`}
-            >
-              <div className="my-2 cursor-pointer whitespace-nowrap font-semibold text-black ">
-                {category.posts ? (
-                  <div className="group-two">
-                    <h1 className="">
-                      <div className="mr-1 inline-block group-two-hover:rotate-90">
-                        &rsaquo;
-                      </div>
-                      {category.name}
-                    </h1>
-                    <div className="container hidden flex-wrap  bg-blue-200 ease-in-out group-two-hover:flex group-two-hover:delay-1000 ">
-                      {category.posts.map((post) => (
-                        <NavLink
-                          key={post.slug}
-                          to={`${language.slug}/${category.slug}/${post.slug}`}
-                        >
-                          <span>{post.title}</span>
-                        </NavLink>
-                      ))}
+            <div className="group-two whitespace-nowrap font-semibold text-black ">
+              <NavLink
+                key={category.slug}
+                to={`${language.slug}/${category.slug}`}
+              >
+                <h1 className="">
+                  {category.posts && (
+                    <div className="mr-1 inline-block group-two-hover:rotate-90">
+                      &rsaquo;
                     </div>
+                  )}
+                  {category.name}
+                </h1>
+              </NavLink>
+              <div className="my-2 cursor-pointer  ">
+                {category.posts && (
+                  <div className="container hidden flex-wrap  bg-blue-200 ease-in-out group-two-hover:flex group-two-hover:delay-1000 ">
+                    {category.posts.map((post) => (
+                      <NavLink
+                        key={post.slug}
+                        to={`${language.slug}/${category.slug}/${post.slug}`}
+                      >
+                        <span className="ml-3">&#8226; {post.title}</span>
+                      </NavLink>
+                    ))}
                   </div>
-                ) : (
-                  category.name
                 )}
               </div>
-            </NavLink>
+            </div>
           ))}
         </div>
       </div>
