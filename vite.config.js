7864fb24b72396a49230e05ecea8418fa40b958e
streetRAT <psnonlineshit@gmail.com>
2022-10-18 16:37:50 +0200

    added regex to syntax highlighting

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [
     react(),
     prismjs({
-      languages: ["json", "rust", "javascript"],
+      languages: ["json", "rust", "javascript", "regex"],
       plugins: [
         "line-highlight",
         "line-numbers",
