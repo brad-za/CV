b45e037830da5fa9eb13cd956ee047fbbeba374a
putintin420 <psnonlineshit@gmail.com>
2022-11-29 11:01:39 +0000

    adds hot reloading to dev container.

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,26 @@ import react from "@vitejs/plugin-react";
 import prismjs from "vite-plugin-prismjs";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    prismjs({
-      languages: ["json", "rust", "javascript", "regex"],
-      plugins: [
-        "line-highlight",
-        "line-numbers",
-        "show-language",
-        "highlight-keywords",
-        "copy-to-clipboard",
-        "previewers",
-        "match-braces",
-      ],
-      theme: "twilight",
-      // languages: 'all',
-    }),
-  ],
+	plugins: [
+		react(),
+		prismjs({
+			languages: ["json", "rust", "javascript", "regex"],
+			plugins: [
+				"line-highlight",
+				"line-numbers",
+				"show-language",
+				"highlight-keywords",
+				"copy-to-clipboard",
+				"previewers",
+				"match-braces",
+			],
+			theme: "twilight",
+			// languages: 'all',
+		}),
+	],
+	server: {
+		watch: {
+			usePolling: true,
+		},
+	},
 });
